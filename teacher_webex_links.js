2686a6f51c4ded3c578d93596bd7dbb64a005786
tethragon <39238674+tethragon@users.noreply.github.com>
2026-01-03 13:26:37 +0200

    Add files via upload

---
+++ teacher_webex_links.js
@@ -0,0 +1,43 @@
+/* * Ημερομηνία Δημιουργίας/Ενημέρωσης: 03/01/2026, 13:25
+ * Το αρχείο περιέχει τους συνδέσμους Webex των εκπαιδευτικών.
+ */
+
+window.RAW_LINKS = `
+ΑΓΓΕΛΟΥ ΤΖΩΡΤΖΙΝΑ,aggelou.g
+ΑΘΑΝΑΣΙΟΥ ΜΑΡΙΑ-ΧΡΙΣΤΙΝΑ,athanasiou.mc
+ΑΛΕΞΟΠΟΥΛΟΥ ΕΙΡΗΝΗ,alexopoulou.e
+ΑΝΔΡΙΑΝΟΠΟΥΛΟΥ ΠΟΛY,andrianopoulou.p
+ΑΝΔΡΙΟΠΟΥΛΟΥ ΔΗΜΗΤΡΑ,andriopoulou_d
+ΑΡΤΑΒΑΝΗ ΘΕΟΔΩΡΑ,artavani.t
+ΒΑΣΙΛΑ ΚΑΤΕΡΙΝΑ,vasila.k
+ΒΕΝΑΡΔΟΥ ΧΡΙΣΤΙΝΑ,venardou.ch
+ΒΟΤΑΝΟΠΟΥΛΟΥ ΕΥΓΕΝΙΑ,votanopoulou.e
+ΒΡΕΤΤΑΚΟΥ ΒΑΡΒΑΡΑ,vrettakou.v
+ΓΕΡΑΝΗ ΧΑΡΙΚΛΕΙΑ,gerani.c
+ΓΙΑΚΟΥΒΗ ΚΩΝΣΤΑΝΤΙΝΑ,giakouvi.k
+ΓΙΑΝΝΑΚΗ ΑΝΝΑ,giannaki.an
+ΔΗΜΗΤΡΟΠΟΥΛΟΥ ΔΗΜΗΤΡΑ,dimitropoulou.d
+ΖΕΡΒΑ ΕΙΡΗΝΗ,zerva.e
+ΚΑΛΛΙΑΝΤΑ ΜΑΡΙΑ,kallianta.m
+ΚΑΤΣΑ ΝΑΝΣΥ,katsa.a
+ΚΗΤΤΑ ΕΛΙΝΑ,kitta.e
+ΚΟΚΚΟΛΑΤΟΥ ΑΛΙΝΑ,kokkolatou.a
+ΚΟΝΤΟΥ ΑΝΘΗ,kontou.a
+ΚΥΡΙΑΚΗ ΚΑΤΕΡΙΝΑ,kiriaki.k
+ΛΟΡΕΝΤΖΟΥ ΖΩΗ,lorentzou.z
+ΜΑΘΙΟΥΔΑΚΗ ΕΥΑΓΓΕΛΙΑ,mathioudaki.e
+ΜΗΛΙΟΠΟΥΛΟΥ ΖΩΗ,miliopoulou.z
+ΜΟΥΡΙΚΗ ΚΑΤΕΡΙΝΑ,mouriki.a
+ΜΠΑΡΛΑΣ ΝΙΚΟΛΑΟΣ,barlas.n
+ΜΠΟΥΡΔΑΚΟΥ ΜΑΡΙΖΑ,bourdakou.m
+ΜΥΤΑΚΙΔΗ ΚΑΤΕΡΙΝΑ,mytakidi.k
+ΠΕΤΡΑΚΗΣ ΓΕΩΡΓΙΟΣ,petrakis.g
+ΣΟΛΑΚΑΚΗ ΧΡΙΣΤΙΝΑ,solakaki.c
+ΣΠΥΡΟΠΟΥΛΟΥ ΜΠΕΤΥ,spyropoulou.p
+ΣΤΕΦΟΥ ΑΣΗΜΙΝΑ,astefou
+ΣΥΚΙΩΤΗ ΧΡΙΣΤΙΝΑ,sykioti.c
+ΤΣΙΡΟΥ ΟΥΡΑΝΙΑ,tsirou.o
+ΦΡΑΓΚΟΥΛΗ ΕΙΡΗΝΗ,fragouli.e
+ΧΑΤΖΗΙΩΑΝΝΟΥ ΔΕΣΠΟΙΝΑ,chatziioannou.d
+ΧΩΡΙΑΝΟΠΟΥΛΟΣ ΚΩΝΣΤΑΝΤΙΝΟΣ,chorianopoulos.k
+`;
